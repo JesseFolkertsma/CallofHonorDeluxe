24ea040d48c444298ed9c5e64ab2e9283d9dfe01
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-12-20 11:05:23 +0100

    MULTIPLAYER

---
+++ UnityProjects/Call_of_Honor_Deluxe/Assets/COHD/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDistable;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDistable.Length; i++)
+            {
+                componentsToDistable[i].enabled = false;
+            }
+        }
+    }
+}
