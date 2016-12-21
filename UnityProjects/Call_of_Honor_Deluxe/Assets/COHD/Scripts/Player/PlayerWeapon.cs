46c0fbf6cf6bc2d72a3825448c0fc3f1451b7d68
JesseFolkertsma <jesse.folkertsma@gmail.com>
2016-12-21 15:38:18 +0100

    working_on_Gunmechanics

---
+++ UnityProjects/Call_of_Honor_Deluxe/Assets/COHD/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour {
+
+    public Weapon weapon;
+
+    void Update()
+    {
+        if (weapon != null)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                weapon.Attack();
+            }
+        }
+    }
+}
